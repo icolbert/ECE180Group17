815b4861c885191509874b3c56a378d8883308d7
icolbert <ian.colbert711@gmail.com>
2018-02-07 21:31:30 -0800

    Started working on coding structure

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+
+from utils import ian
+
+if __name__ == '__main__':
+	ian.load_data('here')
---
+++ utils/ian.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+
+def load_data(x):
+	assert isinstance(x, str)
+	print x
+
+if __name__ == '__main__':
+	load_data('here')
