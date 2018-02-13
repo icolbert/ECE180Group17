54f528731bbb58884a8fe523312996d575ad173b
icolbert <ian.colbert711@gmail.com>
2018-02-12 22:04:37 -0800

    ian.py has a class that loads the data in as a pandas DataFrame and has a couple funcitons that can filter the data with a dictionary feed

--- utils/ian.py
+++ utils/ian.py
@@ -12,14 +12,40 @@ class InputData:
 
 	def load_data(self, fname, read_columns=None):
 
-		assert isinstance(fname, str), "fname is not a string: %s" % fname
+		assert isinstance(fname, str), "fname is not a string: {0}".format(type(fname))
 		assert  (fname in [i for i in glob.glob('data/*.csv')]), "fname is not in data/"
 
-		data = pd.read_csv(fname)
+		self.data = pd.read_csv(fname)
 		if read_columns:
-			self.data = data.filter(items=read_columns, axis=1)
+			self.data = self.data.filter(items=read_columns, axis=1)
 		else:
-			self.data = data.filter(items=self.read_columns, axis=1)
+			self.data = self.data.filter(items=self.read_columns, axis=1)
+
+		N = self.data.sum()
+		bad_columns = list(set(N[N == 0].index).union(set(data) - set(N.index)))
+		self.data = self.data.drop(bad_columns, axis=1)
+
+	def find_rows(self, filter_dict={'geoname':'California'}):
+
+		assert isinstance(filter_dict, dict), "filter_dict is not a dictionary: {0}".format(type(filter_dict))
+		
+		x = self.data
+		for key, value in filter_dict.items():
+			if isinstance(value, list):
+				y = pd.DataFrame(data=[])
+				for v in value:
+					try:
+						y = pd.concat([x[x[key] == str(v)], y], ignore_index=True)
+					except Exception as e:
+						print e
+				x = y
+			else:
+				try:
+					x = x[x[key] == value]
+				except Exception as e:
+					print e
+
+		return x
 
 if __name__ == '__main__':
 
@@ -29,8 +55,17 @@ if __name__ == '__main__':
 		'poprate', 'avmttotal'
 		]
 
-	xdata = InputData()
-	xdata.load_data('data/road-traffic-injuries-2002-2010.csv', read_columns=filter_items)
+	filter_dict = {
+	'geoname': 'California',
+	'mode': 'Bus',
+	'severity': 'Killed',
+	'reportyear': range(2002,2011)
+	}
 
-	xdata.data.to_csv('sample.csv')
+	xdata = InputData()
+	xdata.load_data('data/road-traffic-injuries-2002-2010.csv')
+	xs = xdata.find_rows(filter_dict)
+
+	xs.to_csv('sample.csv')
+
 
