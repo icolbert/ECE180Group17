7b84f8011c9d4b0c0b0a202ecf5698245469ed91
icolbert <ian.colbert711@gmail.com>
2018-03-06 14:24:23 -0800

    Added annual miles traveled

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
 	filter_dict = {
 	'geoname': 'San Diego',
-	'severity': 'Killed',
+	'severity': 'Severe Injury',
 	'mode': None,
 	'reportyear': range(2002,2011)
 	}
@@ -32,27 +32,29 @@ if __name__ == '__main__':
 
 
 	### Applying same code to Annual Miles Travelled
-
+	
 	filter_items = [
-		'geoname', 'reportyear', 'region_code'
+		'geoname', 'reportyear', 'region_code', 'groupquarters',
 		'mode', 'ratio_type', 'ratio', 'county_name'
 		]
 
 	filter_dict = {
 	'geoname': 'California',
+	'ratio_type': 'Miles Per Square Mile',
 	'mode': None,
 	'reportyear': range(2002,2011)
 	}
 	miles_data = InputData()
-	miles_data.load_data('data/annual-miles-traveled-2002-2010.csv')
+	miles_data.load_data('data/annual-miles-traveled-2002-2010.csv', read_columns=filter_items)
 	xs = {}
 	for m in ['Bicyclist', 'Pedestrian', 'Vehicle']:
 		filter_dict['mode'] = m
-		xs.update({m: xdata.filter_data(xdata.find_rows(filter_dict))})
+		xs.update({m: miles_data.filter_data(miles_data.find_rows(filter_dict))})
 		print('\n')
 
 	for key, data in xs.items():
-		a = BuildModel(data, ylabel='Miles Traveled per Square Mile')
+		data.to_csv('test.csv')
+		a = BuildModel(data,ydim='ratio', ylabel='Miles Traveled per Square Mile')
 		#a.LinReg(key)
 		#a.QuadReg(key)
 		a.all(key)
